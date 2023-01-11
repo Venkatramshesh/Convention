49b8cfe844b59a999ea41c86d5ff45fe1d8c1b0d
Venkat <114759422+kris1878@users.noreply.github.com>
2023-01-11 10:18:16 -0500

    Update application.py

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, jsonify, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from flask_sqlalchemy import SQLAlchemy
@@ -17,47 +17,61 @@ application.config['SECRET_KEY'] = 'jCOo4PAnmU6A0j2lpKeI-A'
 
 AWS_region = 'us-east-1'
 
+boto3.setup_default_session(profile_name='iamadmin-general')
+dynamodb = boto3.resource(service_name='dynamodb')
 
-dynamodb = boto3.resource('dynamodb', region_name=AWS_region)
+tablename="newraffletable"
 
-def create_raffles_table(dynamodb=None):
-    dynamodb = boto3.resource('dynamodb', region_name=AWS_region)
-    #dynamodb = boto3.resource('dynamodb', endpoint_url="http://localhost:8000")
-    # Table defination
-    table = dynamodb.create_table(
-        TableName='raffle',
-        KeySchema=[
-            {
-                'AttributeName': 'name',
-                'KeyType': 'HASH'  # Partition key
-            },
-            {
-                'AttributeName': 'email',
-                'KeyType': 'RANGE'  # Sort key
+
+def create_raffles_table(dynamodb):
+
+    table_names = [table.name for table in dynamodb.tables.all()]
+
+    if tablename in table_names:
+        print('table', tablename, 'exists')
+
+    #print(response)
+
+    else:
+
+        #dynamodb = boto3.resource('dynamodb', endpoint_url="http://localhost:8000")
+        # Table defination
+        table = dynamodb.create_table(
+            TableName=tablename,
+
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'id',
+                    # AttributeType defines the data type. 'S' is string type and 'N' is number type
+                    'AttributeType': 'N'
+                },
+                {
+                    'AttributeName': 'name',
+                    # AttributeType defines the data type. 'S' is string type and 'N' is number type
+                    'AttributeType': 'S'
+                },
+
+            ],
+
+            KeySchema=[
+                {
+                    'AttributeName': 'id',
+                    'KeyType': 'HASH'  # Partition key
+                },
+                {
+                    'AttributeName': 'name',
+                    'KeyType': 'RANGE'  # Sort key
+                }
+
+            ],
+            ProvisionedThroughput={
+                # ReadCapacityUnits set to 10 strongly consistent reads per second
+                'ReadCapacityUnits': 10,
+                'WriteCapacityUnits': 10  # WriteCapacityUnits set to 10 writes per second
             }
-        ],
-        AttributeDefinitions=[
-            {
-                'AttributeName': 'name',
-                # AttributeType defines the data type. 'S' is string type and 'N' is number type
-                'AttributeType': 'S'
-            },
-            {
-                'AttributeName': 'email',
-                'AttributeType': 'S'
-            },
-        ],
-        ProvisionedThroughput={
-            # ReadCapacityUnits set to 10 strongly consistent reads per second
-            'ReadCapacityUnits': 10,
-            'WriteCapacityUnits': 10  # WriteCapacityUnits set to 10 writes per second
-        }
-    )
+        )
 
-try:
-    create_raffles_table()
-except:
-    print("Table already exixts")
+
 
 # class comments(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
@@ -71,26 +85,43 @@ except:
 def home():
     form = SubmitForm()
     flag = True
-    raffle_inputs = dynamodb.Table('raffle')
+    raffle_inputs = dynamodb.Table(tablename)
     if form.validate_on_submit():
-        response = raffle_inputs.scan()['Items']
-        for items in response:   # Check to see if that email is already registered
-            if form.email.data==items['email']:
-                flag = False
-                return render_template('error.html')
-                print("User already registered")
-
-        if flag:
-           raffle_inputs.put_item(Item={
+        try:
+            response = raffle_inputs.scan()(Item={'id':1, 'name':form.name.data})
+            flag = False
+            return render_template('error.html')
+            print("User already registered")
+        except:
+           raffle_inputs.put_item(Item={'id':1,
            'email': form.email.data,
            'name': form.name.data })
     return render_template('index.html',form=form)
+
+@application.route('/scan',methods=["GET","POST"])
+def scan():
+    flag = True
+    raffle_outputs = dynamodb.Table(tablename)
+    raffle_outputs.get_item(Item={'id':1})
+    return (jsonify(raffle_outputs))
+
+@application.route('/query',methods=["GET","POST"])
+def query():
+    recname = request.args.get('name')
+    flag = True
+    raffle_outputs = dynamodb.Table(tablename)
+    raffle_outputs.query(Item={'id':1, 'name':recname})
+    return (jsonify(raffle_outputs))
 
 @application.route('/error')
 def error():
     return render_template('error.html')
 
 if __name__=="__main__":
-     application.run(debug=True)
+    create_raffles_table(dynamodb)
+    application.run()
+
+
+
 
 
